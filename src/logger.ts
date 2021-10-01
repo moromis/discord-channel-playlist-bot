a7b020b7e9203e9957400a71ddd829330dfe74cc
Kevin Ulrich <kzulrich@gmail.com>
2021-09-30 17:44:22 -0700

    add debug flag for logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,1 +1,12 @@
-export const logger = console;
+import * as _ from "lodash";
+
+const noLogger: Console = {
+  ...console,
+  info: _.noop,
+  error: _.noop,
+  log: _.noop,
+};
+
+console.log("debug: ", !!process.env.DEBUG);
+
+export const logger: Console = process.env.DEBUG ? console : noLogger;
